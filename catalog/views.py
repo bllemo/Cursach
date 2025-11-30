065179fb1081ad3333b70937015b41787d4253b2
bllemo <podkovskij.maks1@gmail.com>
2025-12-01 03:25:53 +0700

    comment

--- catalog/views.py
+++ catalog/views.py
@@ -9,6 +9,10 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     num_genres = Genre.objects.count()
 
     search_word = "W"
@@ -21,6 +25,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'num_books_with_word': num_books_with_word,
+        'num_visits': num_visits,
         'search_word': search_word,
     }
 
@@ -46,3 +51,4 @@ class AuthorListView(generic.ListView):
 class AuthorDetailView(generic.DetailView):
     model = Author
     template_name = 'catalog/author_detail.html'
+
